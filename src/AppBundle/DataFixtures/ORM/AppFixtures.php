0364b4471dc85823df30091f84aa1df9e7180dd7
mjourdan <marine.misser@gmail.com>
2022-11-10 10:00:07 +0100

    #11 ajout fixtures et user anonymous

--- src/AppBundle/DataFixtures/ORM/AppFixtures.php
+++ src/AppBundle/DataFixtures/ORM/AppFixtures.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\DataFixtures;
+namespace AppBundle\DataFixtures;
 
 use AppBundle\Entity\User;
+use AppBundle\Entity\Task;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -10,17 +11,55 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-    $user = new User();
+        $anonymousUser = new User();
 
-    $user->setUsername('jean');
-    $password='proutprout';
-    $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
-    $user->setPassword($password);
-    $user->setEmail('prout@prout.fr');
-    $user->setRoles(['ROLE_ADMIN']);
+        $anonymousUser->setUsername('anonyme');
+        $password='anonyme';
+        $password = $this->container->get('security.password_encoder')->encodePassword($anonymousUser, $anonymousUser->getPassword());
+        $anonymousUser->setPassword($password);
+        $anonymousUser->setEmail('anonyme@anonyme.fr');
+        $anonymousUser->setRoles(['ROLE_USER']);
 
-    $manager->persist($user);
+        $manager->persist($anonymousUser);
 
-    $manager->flush();
+        $user1 = new User();
+
+        $user1->setUsername('maurice');
+        $password='maurice';
+        $password = $this->container->get('security.password_encoder')->encodePassword($user1, $user1->getPassword());
+        $user1->setPassword($password);
+        $user1->setEmail('maurice@maurice.fr');
+        $user1->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($user1);
+
+        $task= new Task();
+        $task->setTitle ('coucou');
+        $task->setContent('je suis donc un utilisateur anonyme');
+
+        $manager->persist($task);
+
+        $task1= new Task();
+        $task1->setTitle ('bonjour');
+        $task1->setContent('version à jour de symfony');
+        $task1->setAuthor($user1);
+    
+        $manager->persist($task1);
+
+        $doctrine = $this->container->get('doctrine');
+        $taskRepository = $doctrine->getRepository(Task::class);
+
+        $manager->flush();
+        
+        $tasks= $taskRepository->findAll();
+        foreach($tasks as $task){
+            if (!$task->getAuthor()){
+                $task->setAuthor($anonymousUser); 
+            }
+            $manager->persist($task);
+        }
+
+        $manager->flush();
+
     }
 }
