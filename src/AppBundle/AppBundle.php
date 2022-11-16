fe8b4602560028c1d33a10e15c0be50d7179a658
mjourdan <marine.misser@gmail.com>
2022-11-16 09:25:09 +0100

    migration in progress

---
+++ src/AppBundle/AppBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
+class AppBundle extends Bundle
+{
+
+}
